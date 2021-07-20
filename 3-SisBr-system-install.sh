300215063cc7c7d082e9e6e0858e77981cafb6bf
Jumper Luko <jumper.luko@gmail.com>
2021-07-20 16:08:46 -0300

    Gerados cases para verificar se existe arquivo de instalação

--- 3-SisBr-system-install.sh
+++ 3-SisBr-system-install.sh
@@ -1,17 +1,90 @@
+#| Mostrar sim ou não para funções
+sim_nao() {
+    while true; do
+        read -p "$* [s/n]: " sn
+        case $sn in
+            [Ss]*) sim_ou_no="sim" && return 0  ;;  
+            [Nn]*) printf "Cancelado\n\n" ; sim_ou_nao="nao" && return  1 ;;
+        esac
+        sim_ou_no="null"
+    done
+}
+
 # Local onde ficará os programas e instaladores
 echo "Criando pasta do wine e instaladores"
 sudo mkdir /opt/wine/
 sudo mkdir /opt/wine/instaladores/
 
-# Criar diretório do wine
-echo "Criando diretório do wine em /opt/wine/"
+# Gerando firetórios do wine
+echo "Gerando diretórios do wine em /opt/wine/"
 sudo WINEPREFIX=/opt/wine/ wine wineboot
 
 # copiar instalador para /opt/wine/instaladores/
-#! Futuramente tem que copiar somente se não existir o arquivo em instaladores
-echo "copiando instalador do Sisbr e AdobeAir para pasta de instaladores"
-sudo cp AdobeAIRInstaller.exe /opt/wine/instaladores/
-sudo cp Sisbr20.exe /opt/wine/instaladores/
+adobeAIR=AdobeAIRInstaller.exe
+SisBr=Sisbr20.exe
+
+# Verificando se existe o arquivo do adobeAIR na pasta local
+if [ -e $adobeAIR ] && ! [ -e /opt/wine/instaladores/$adobeAIR ]; then
+	echo "copiando instalador AdobeAir para pasta de instaladores"
+    sudo cp $adobeAIR /opt/wine/instaladores/
+if [ -e $adobeAIR ] && [ -e /opt/wine/instaladores/$adobeAIR ]; then
+    echo "Instalador já existe em /opt/wine/instaladores/, deseja sobrescrever?"
+    adobeAIRSobrescrever(){
+        sim_nao;if [ "$sim_ou_nao" == "nao" ];then
+            return
+        fi
+        echo "copiando instalador AdobeAir para pasta de instaladores"
+        sudo cp $adobeAIR /opt/wine/instaladores/
+    }
+    adobeAIRSobrescrever
+if ! [ -e $adobeAIR ] && [ -e /opt/wine/instaladores/$adobeAIR ]; then
+    echo "Não existe instalador com nome $adobeAIR nesta pasta, porém existe no sistema e será usada esta"
+    echo "deseja continuar?"
+    sim_nao;if [ "$sim_ou_nao" == "nao" ];then
+        exit
+    fi
+if ! [ -e $adobeAIR ] && [ -e /opt/wine/instaladores/$adobeAIR ]; then
+    echo "não existe instaladores do adobeAIR, por favor jogue um arquivo nesta pasta com nome $adobeAIR"
+    echo "este arquivo encerrará a execução"
+    sleep 5
+    exit
+else
+    echo "Falha inesperada ao verificar arquivos do adobeAIR"
+    echo "O script continuará em 15 segundos"
+    sleep 15
+fi
+
+# Verificando se existe o arquivo do SisBr na pasta local
+if [ -e $SisBr ] && ! [ -e /opt/wine/instaladores/$SisBr ]; then
+	echo "copiando instalador SisBr para pasta de instaladores"
+    sudo cp $SisBr /opt/wine/instaladores/
+if [ -e $SisBr ] && [ -e /opt/wine/instaladores/$SisBr ]; then
+    echo "Instalador já existe em /opt/wine/instaladores/, deseja sobrescrever?"
+    SisBrSobrescrever(){
+        sim_nao;if [ "$sim_ou_nao" == "nao" ];then
+            return
+        fi
+        echo "copiando instalador SisBr para pasta de instaladores"
+        sudo cp $SisBr /opt/wine/instaladores/
+    }
+    SisBrSobrescrever
+if ! [ -e $SisBr ] && [ -e /opt/wine/instaladores/$SisBr ]; then
+    echo "Não existe instalador com nome $SisBr nesta pasta, porém existe no sistema e será usada esta"
+    echo "deseja continuar?"
+    sim_nao;if [ "$sim_ou_nao" == "nao" ];then
+        exit
+    fi
+if ! [ -e $SisBr ] && [ -e /opt/wine/instaladores/$SisBr ]; then
+    echo "não existe instaladores do SisBr, por favor jogue um arquivo nesta pasta com nome $SisBr"
+    echo "este arquivo encerrará a execução"
+    sleep 5
+    exit
+else
+    echo "Falha inesperada ao verificar arquivos do SisBr"
+    echo "O script continuará em 15 segundos"
+    sleep 15
+fi
+
 
 # Instalar AdobeAir na pasta do sistema (precaução)
 echo "Instalando AdobeAIR"
--- testes/if-test.sh
+++ testes/if-test.sh
@@ -1,22 +1,87 @@
 #!/bin/bash
 
-echo "File is open"
-yes_no() {
-	while true; do
-		read -p "$* [y/n]" yn
-		case $yn in
-			[Yy]*) yes_or_no="yes" && return 0 ;;
-			[Nn]*) printf "Aborted\n\n" ; yes_or_no="no" && return 1;;
-		esac
-		yes_or_no="null"
-	done
+#| Mostrar sim ou não
+sim_nao() {
+    while true; do
+        read -p "$* [s/n]: " sn
+        case $sn in
+            [Ss]*) sim_ou_no="sim" && return 0  ;;  
+            [Nn]*) printf "Cancelado\n\n" ; sim_ou_nao="nao" && return  1 ;;
+        esac
+        sim_ou_no="null"
+    done
 }
-echo "end of funcion"
-#yes_no
-[ -z yes_no ] && return
-#functionToBreak(){
-#	echo "functionToBreak start"
-#	if [-z yes_no] && return
-#}
-#functionToBreak
-echo "end file"
+
+funcao1(){
+	sim_nao;if [ "$sim_ou_nao" == "nao" ];then
+        return
+    fi
+	printf "Retornou\n\n"
+}
+# funcao1
+sim_nao;if [ "$sim_ou_nao" == "nao" ];then
+	exit
+fi
+echo "não saiu"
+
+
+
+# if ! [ -e "text.txt" ];then
+# 	echo "no file"
+# fi
+
+# testCase=abcd
+# test1=one 
+# test2=two
+# if [ $testCase = "123" ];then
+# 	echo "123"
+# elif [ $testCase = "abc" ];then
+# 	echo "abc"
+# elif [ $test1="one" ] && [ $test2="two" ];then
+# 	echo "one and two"
+# else
+# 	echo "nothing"
+# fi
+
+# testCase=abc
+# case $file in
+
+# 	123)
+# 		echo "123";;
+# 	abc)
+# 		echo "abc";;
+# 	xyz | 987)
+# 		echo "xyz or 987";;
+
+# esac
+
+
+# file=comandos-zorin-sicoob.txt-
+# if [ -e $file ]
+# then
+# 	echo "achei"
+# else
+# 	echo "não achei"
+# fi
+
+
+# echo "File is open"
+# yes_no() {
+# 	while true; do
+# 		read -p "$* [y/n]" yn
+# 		case $yn in
+# 			[Yy]*) yes_or_no="yes" && return 0 ;;
+# 			[Nn]*) printf "Aborted\n\n" ; yes_or_no="no" && return 1;;
+# 		esac
+# 		yes_or_no="null"
+# 	done
+# }
+# echo "end of funcion"
+# #yes_no
+# [ -z yes_no ] && return
+# #functionToBreak(){
+# #	echo "functionToBreak start"
+# #	if [-z yes_no] && return
+# #}
+# #functionToBreak
+# echo "end file"
