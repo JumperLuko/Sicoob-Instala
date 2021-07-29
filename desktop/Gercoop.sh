cc1353a2161a5b376228797c277fac37f97f8d95
JumperLuko <jumper.luko@gmail.com>
2021-07-29 18:03:32 -0300

    +Arquivo de execução do Gercoop Ele ainda não está linkado nos instaladores, necessitarei reorganizar os diretórios

---
+++ desktop/Gercoop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+homeGercoop="$home/.local/share/gercoop/"
+runGercoop(){ (cd $home/.local/share/gercoop/; wine /opt/wine/drive_c/GerCoop/GerCoop.exe) }
+
+# Executa na pasta do Gercoop, se não houve, criará
+if [ -d "$homeGercoop" ]; then
+	runGercoop
+elif ! [ -e "$homeGercoop" ]; then
+	mkdir "$homeGercoop"; runGercoop
+elif [ ! -d "$homeGercoop" ] && [ -e "$homeGercoop" ]; then
+	(cd "$home/.local/share/" && mv "gercoop" "gercoop-file")
+	mkdir "$homeGercoop"; runGercoop
+fi
