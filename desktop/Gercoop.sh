56feb0c382e85d513a0284cba2801406146e8d78
JumperLuko <jumper.luko@gmail.com>
2021-07-30 14:53:13 -0300

    ~Corrigido caminho do diretório local no arquivo de execução do Gercoop

--- desktop/Gercoop.sh
+++ desktop/Gercoop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-homeGercoop="$home/.local/share/gercoop/"
-runGercoop(){ (cd "$home/.local/share/gercoop/"; wine "/opt/sicoob/wine/drive_c/GerCoop/GerCoop.exe") }
+homeGercoop="$home.local/share/gercoop/"
+runGercoop(){ (cd "$homeGercoop"; wine "/opt/sicoob/wine/drive_c/GerCoop/GerCoop.exe") }
 
 # Executa na pasta do Gercoop, se não houve, criará
 if [ -d "$homeGercoop" ]; then
@@ -9,6 +9,6 @@ if [ -d "$homeGercoop" ]; then
 elif ! [ -e "$homeGercoop" ]; then
 	mkdir "$homeGercoop"; runGercoop
 elif [ ! -d "$homeGercoop" ] && [ -e "$homeGercoop" ]; then
-	(cd "$home/.local/share/" && mv "gercoop" "gercoop-file")
+	(cd "$home.local/share/" && mv "gercoop" "gercoop-file")
 	mkdir "$homeGercoop"; runGercoop
 fi
