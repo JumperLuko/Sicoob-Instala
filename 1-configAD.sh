f597616d0d53ed81e5b8ee20a7e1645aeccfa5d8
jumper luko <jumper.luko@gmail.com>
2021-09-01 17:07:03 -0300

    Pequena correção de espaçamento na exibição

--- 1-configAD.sh
+++ 1-configAD.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Encontrar AD sicoobsmo
-echo "Procurando por AD sicoobsmo.local"
+echo -e "\nProcurando por AD sicoobsmo.local"
 realm discover sicoobsmo.local
 
 # Exibir nome da maquina para o administrador para verificar se é isso mesmo
-echo "Este é o nome da sua maquina, verifique se está correto"
+echo -e "\nEste é o nome da sua maquina, verifique se está correto"
 hostname -f
 
 # Entrar AD
-echo "AD sicoobsmo.local irá ser adicionado, por favor digite a senha de administrador do AD"
+echo -e "\nAD sicoobsmo.local irá ser adicionado, por favor digite a senha de administrador do AD"
 sudo realm join sicoobsmo.local --user=administrador
 
 # Verificar se está conectada ao AD
-echo "Verifique se está conectado ao AD"
+echo -e "\nVerifique se está conectado ao AD"
 realm list sicoobsmo.local
 
 # Sair do AD (é uma anotação)
