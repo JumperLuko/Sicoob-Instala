0fc67125168f0df08d3844c3cf289f3b1d770e12
jumper luko <jumper.luko@gmail.com>
2021-09-03 13:54:12 -0300

    +Criar pasta ao logar no usuário

--- 1-configAD.sh
+++ 1-configAD.sh
@@ -8,6 +8,10 @@ realm discover sicoobsmo.local
 echo -e "\nEste é o nome da sua maquina, verifique se está correto"
 hostname -f
 
+# Criar ao add usuário
+echo -e "\nConfigurando para criar pasta de usuário logar" 
+sudo pam-auth-update --enable mkhomedir
+
 # Entrar AD
 echo -e "\nAD sicoobsmo.local irá ser adicionado, por favor digite a senha de administrador do AD"
 sudo realm join sicoobsmo.local --user=administrador
