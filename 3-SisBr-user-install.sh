b6622c31cb62aa53c188f7d07782ea36a48df3d2
jumper luko <jumper.luko@gmail.com>
2021-09-15 15:30:43 -0300

    ~ Corrigido caminho dos instaladores no 3-SisBr-user-install.sh

--- 3-SisBr-user-install.sh
+++ 3-SisBr-user-install.sh
@@ -5,8 +5,13 @@
 #sleep 10
 #wine wineboot &
 
+adobeAIR=AdobeAIRInstaller.exe
+instaladorPasta="/opt/sicoob/instaladores"
+
 # Verifica se há instalador do adobeAIR e se não está instalado, então ele abre o instalador
 #! Gera atrazo na primeira inicialização do usuário, e o desktop não aparece enquanto não finalizar wine
-if [ -e /opt/wine/instaladores/$adobeAIR ] && ! [ -e ".wine/drive_c/Program Files (x86)/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller.exe" ]; then
-    sleep 5 && wine /opt/wine/instaladores/AdobeAIRInstaller.exe &
+if [ -e "$instaladorPasta"/"$adobeAIR" ] && ! [ -e ".wine/drive_c/Program Files (x86)/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller.exe" ]; then
+    sleep 5 && wine "$instaladorPasta"/"$adobeAIR" &
 fi
+
+unset adobeAIR instaladorPasta
