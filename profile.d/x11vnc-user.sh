a17110269498848e6776998d5ac513894cc097c8
jumper luko <jumper.luko@gmail.com>
2021-09-28 11:44:19 -0300

    - removido loop para não ficar gerando texto no terminal

--- profile.d/x11vnc-user.sh
+++ profile.d/x11vnc-user.sh
@@ -1,1 +1,1 @@
-/usr/bin/x11vnc -forever -loop -noxdamage -repeat -rfbauth /opt/sicoob/passwdvnc.pwd -autoport 5900 -shared -o .local/share/x11vnc.log &
+/usr/bin/x11vnc -forever -noxdamage -repeat -rfbauth /opt/sicoob/passwdvnc.pwd -autoport 5900 -shared -o .local/share/x11vnc.log -q &
