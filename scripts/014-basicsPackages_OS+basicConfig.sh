8fadfe33f43671d27918cf46c0504c0ffc2583de
jumper luko <jumper.luko@gmail.com>
2022-01-06 15:53:59 -0300

    - fallocate não existe

--- scripts/014-basicsPackages_OS+basicConfig.sh
+++ scripts/014-basicsPackages_OS+basicConfig.sh
@@ -44,7 +44,7 @@ sudo apt upgrade -y
 # `gdebi` para instala pacotes deb
 # `ntpdate` ferramenta para verificar se o horário do PC está sincronizado com a internet
 # `menulibre` Para icones no sistema funcionarem. !Algo sem isso não está deixando os icones funcionarem
-sudo apt install sssd-ad sssd-tools realmd adcli samba smbclient wine wine64 wine32:i386 winetricks mono-dbg x11vnc git git-gui libxtst6:i386 icedtea-netx openssh-server unattended-upgrades gdebi ntpdate ttf-mscorefonts-installer winbind menulibre fallocate -y
+sudo apt install sssd-ad sssd-tools realmd adcli samba smbclient wine wine64 wine32:i386 winetricks mono-dbg x11vnc git git-gui libxtst6:i386 icedtea-netx openssh-server unattended-upgrades gdebi ntpdate ttf-mscorefonts-installer winbind menulibre -y
 
 # Clonar repositório Github, novamente caso o pacote não tenha sido instalado antes
 source $scriptsDir/4-SicoobInstala-clone.sh
