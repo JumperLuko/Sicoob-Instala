53858206e839a4c03277038ebe6ad4ad8694a32b
jumper luko <jumper.luko@gmail.com>
2022-01-21 16:35:10 -0300

    ~ fix sisbr run

--- desktop/run/SisBr.sh
+++ desktop/run/SisBr.sh
@@ -10,13 +10,17 @@ instaladorPasta="/opt/sicoob/instaladores"
 if [ -e "$instaladorPasta"/"$adobeAIR" ] && ! [ -e ".wine/drive_c/Program Files (x86)/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller.exe" ]; then
     gnome-terminal -- bash -c 'adobeAIR=AdobeAIRInstaller.exe && instaladorPasta="/opt/sicoob/instaladores" &&\
  echo -e "Aperte enter para Instalar \e[5;1;32mdependencia do SisBR\e[0m (Adobe AIR)" && read &&\
- wine "$instaladorPasta"/"$adobeAIR &&\
- wine /opt/sicoob/wine/drive_c/Sisbr\ 2.0/Sisbr\ 2.0.exe &"'
+ wine "$instaladorPasta"/"$adobeAIR"'
+    # Gnome terminal roda em fundo por padrão, o código abaixo não vai esperar o gnome terminal para finalizar 
+    if [ -e ".wine/drive_c/Program Files (x86)/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller.exe" ]; then
+        wine /opt/sicoob/wine/drive_c/Sisbr\ 2.0/Sisbr\ 2.0.exe
+    fi
 else
     wine /opt/sicoob/wine/drive_c/Sisbr\ 2.0/Sisbr\ 2.0.exe
+    echo bah
 fi
 
-if ! [ -e "$HOME/.wine/drive_c/windows/Fonts/corefonts.installed" ]; then
+if ! [ -e "$HOME/.wine/drive_c/windows/Fonts/corefonts.installed" ] && [ -e "$HOME/.wine/" ]; then
     winetricks corefonts &
 fi
 
