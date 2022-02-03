9767db94329c8868324a8c4c8bd4ed200f482f86
jumper luko <jumper.luko@gmail.com>
2022-02-03 11:43:10 -0300

    ~ remove chrome repo

--- scripts/0-repositoryPrograms-install.sh
+++ scripts/0-repositoryPrograms-install.sh
@@ -12,6 +12,8 @@ fi
 # Google Chrome (repo)
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+deb [arch=amd64] http:\/\/dl.google.com\/linux\/chrome\/deb\/ stable main
+
 
 # Teams
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - 
@@ -32,7 +34,9 @@ sudo add-apt-repository ppa:yann1ck/onedrive -yn
 sudo apt update
 sudo apt install google-chrome-stable teams anydesk teamviewer onedrive
 
+# Remover repositórios que estarão repetidos
 sudo apt-add-repository "deb http://linux.teamviewer.com/deb stable main" --remove
+sudo sed -i -e 's/deb \[arch\=amd64] http:\/\/dl.google.com\/linux\/chrome\/deb\/ stable main//' /etc/apt/sources.list.d/google-chrome.list
 
 # # OnlyOffice
 # # adiciona via flatpak já
