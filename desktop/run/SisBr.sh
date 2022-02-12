78d03c69a1dc6d1f6603d5157ff9ff4609433d4c
JumperLuko <jumper.luko@gmail.com>
2022-02-12 14:19:12 -0300

    + kdialog + Ao executar o SisBr pela primeira vez, o usuário será informado para instalar o AdobeAir! - Não terá mais terminal na execução do SisBr!

--- desktop/run/SisBr.sh
+++ desktop/run/SisBr.sh
@@ -6,18 +6,18 @@ adobeAIR=AdobeAIRInstaller.exe
 instaladorPasta="/opt/sicoob/instaladores"
 
 # Verifica se há instalador do adobeAIR e se não está instalado, então ele abre o instalador
-#! Gera atrazo na primeira inicialização do usuário, e o desktop não aparece enquanto não finalizar wine
 if [ -e "$instaladorPasta"/"$adobeAIR" ] && ! [ -e ".wine/drive_c/Program Files (x86)/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller.exe" ]; then
-    gnome-terminal -- bash -c 'adobeAIR=AdobeAIRInstaller.exe && instaladorPasta="/opt/sicoob/instaladores" &&\
- echo -e "Aperte enter para Instalar \e[5;1;32mdependencia do SisBR\e[0m (Adobe AIR)" && read &&\
- wine "$instaladorPasta"/"$adobeAIR"'
-    # Gnome terminal roda em fundo por padrão, o código abaixo não vai esperar o gnome terminal para finalizar 
+    kdialog --msgbox "Instale o Adobe Air para executar o Sisbr\n\nTalvez precise aguardar um pouco" --title "SisBr 2.0" &
+    wine "$instaladorPasta"/"$adobeAIR"
+    # Executa o SisBr se o Adobe Air tiver instalado
     if [ -e ".wine/drive_c/Program Files (x86)/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller.exe" ]; then
         wine /opt/sicoob/wine/drive_c/Sisbr\ 2.0/Sisbr\ 2.0.exe
     fi
+# Se não tiver instalador, nem instalador, informa o usuário
+elif ! [ -e "$instaladorPasta"/"$adobeAIR" ] && ! [ -e ".wine/drive_c/Program Files (x86)/Adobe/Flash Player/AddIns/airappinstaller/airappinstaller.exe" ]; then
+    kdialog --error "Infelizmente não há AdobeAir no sistema\n\nPor favor contate o administrador do sistema" --title "SisBr 2.0"
 else
     wine /opt/sicoob/wine/drive_c/Sisbr\ 2.0/Sisbr\ 2.0.exe
-    echo bah
 fi
 
 if ! [ -e "$HOME/.wine/drive_c/windows/Fonts/corefonts.installed" ] && [ -e "$HOME/.wine/" ]; then
