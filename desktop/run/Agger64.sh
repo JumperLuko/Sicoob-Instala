af3394d1ee39cd5b9db0d6b822a94010369958a3
jumper luko <jumper.luko@gmail.com>
2022-02-16 17:22:33 -0300

    Agger64?

---
+++ desktop/run/Agger64.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Verifica se há instalador do dotNet452_PTB e se não está instalado, então ele abre o instalador
+# Se tiver tudo ok, inicializa o agger
+dotNet452_PTB=NDP452-KB2901907-x86-x64-AllOS-PTB.exe
+instaladorPasta="/opt/sicoob/instaladores"
+logFile="$HOME/.cache/SicoobInstala.log"
+
+dotnet452Instalado="$HOME/.wine/drive_c/windows/Microsoft.NET/Framework/v4.0.30319/Microsoft.Windows.ApplicationServer.Applications.45.man"
+dotnet452PTBRInstalado="$HOME/.wine/drive_c/windows/Microsoft.NET/Framework/v4.0.30319/pt-BR/aspnet.mfl"
+
+#! Experimentar migrar para wine 7, utilizando arquitetura x64
+if [ -e "$instaladorPasta"/"$dotNet452_PTB" ] && ! [ -e "$dotnet452Instalado" ]; then
+    kdialog --yesno "Você deseja instalar o Agger?\n\nDurante o processo deverá clicar sim ao o que aparecer\n\nO processo é demorado" --title "Agger"
+    if [ $? = 0 ]; then
+        wineboot 2>> $logFile
+        winetricks dotnet452 2>> $logFile
+        winetricks win7
+        wine "$instaladorPasta"/"$dotNet452_PTB" 2>> $logFile
+        wine /opt/sicoob/instaladores/Instalador-agger.exe
+    fi
+elif ! [ -e "$instaladorPasta"/"$dotNet452_PTB" ] && ! [ -e "$dotnet452Instalado" ]; then
+    kdialog --error "Infelizmente não há dotNet452 ptb no sistema\n\nPor favor contate o administrador do sistema" --title "Agger"
+else
+    wine $HOME/.wine32/drive_c/AggerSeguros/Aggilizador.Application.exe
+fi
+
+if ! [ -e "$wine32Folder/drive_c/windows/Fonts/corefonts.installed" ]; then
+    winetricks corefonts
+fi
+
+unset dotNet452_PTB instaladorPasta wine32Folder dotnet452Instalado dotnet452PTBRInstalado logFile
